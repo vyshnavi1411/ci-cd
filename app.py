4ded8ba6b89495448a2f35f2aca7eac47ab2e242
vyshnavi1411 <vyshnavi.kusukuntla@gmail.com>
2026-05-13 12:26:03 +0530

    Initial commit

--- app.py
+++ app.py
@@ -4,3 +4,4 @@ def add(a, b):
 if __name__ == "__main__":
     print(add(2, 3))
     # Output: 5
+    # You can run this script to see the result of the add function. The test case in test_app.py will also verify that the add function works correctly.
