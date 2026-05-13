8f1cacc6d2249d636a95067b2465a93b80b89826
vyshnavi1411 <vyshnavi.kusukuntla@gmail.com>
2026-05-13 12:32:44 +0530

    new ci/cd

--- app.py
+++ app.py
@@ -1,7 +1,10 @@
-def add(a, b):
-    return a + b
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route("/")
+def home():
+    return "CI/CD Pipeline Working!"
 
 if __name__ == "__main__":
-    print(add(2, 3))
-    # Output: 5
-    # You can run this script to see the result of the add function. The test case in test_app.py will also verify that the add function works correctly.
+    app.run(host="0.0.0.0", port=5000)
